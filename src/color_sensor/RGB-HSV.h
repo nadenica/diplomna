529d1990d1cafd1aa5641f8157d383443dda1432
nadya <107633042+nadenica@users.noreply.github.com>
2023-12-11 12:36:32 +0200

    Delete src/color_sensor/RGB-HSV.h

--- src/color_sensor/RGB-HSV.h
+++
@@ -1,7 +0,0 @@
-typedef struct {
-    float h; // Hue [0, 360]
-    float s; // Saturation [0, 100]
-    float v; // Value [0, 100]
-} HSVColor;
-
-void RGBtoHSV(int r, int g, int b, HSVColor* hsv);
