cb400b8f54a72e2c2c86478d86a0a1b3c4adb551
nadya <107633042+nadenica@users.noreply.github.com>
2024-03-01 23:23:32 +0200

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./App.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
